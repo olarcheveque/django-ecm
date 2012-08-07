38171496fbe176354a0df55dabef712be20d5549
Olivier Larchevêque <olivier.larcheveque@auf.org>
2012-08-07 16:45:05 -0400

    hopl

--- src/django-ecm/ecm/core/views.py
+++ src/django-ecm/ecm/core/views.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, UpdateView, CreateView
 from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import ModelFormMixin
 
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import messages
@@ -40,10 +41,12 @@ class ContentMixin(SingleObjectMixin):
         Fix the model dynamically
         """
         obj = super(ContentMixin, self).get_object(queryset)
-        # TODO move & factorize
+
+        # Setup content Type
         self.content_type = obj.content_type
         self.model = self.content_type.model_class()
         self._setup_settings(obj.content_type.model)
+
         return obj.get_object()
 
 
@@ -53,12 +56,7 @@ class ContentDetailView(ContentMixin, DetailView):
         return ("ecm/%s_detail.html" % self.object.content_type.model,
                 "ecm/folder_detail.html",)
 
-
-class ContentUpdateView(ContentMixin, UpdateView):
-
-    def get_template_names(self):
-        return ("ecm/%s_edit.html" % self.object.content_type.model,
-                "ecm/folder_edit.html",)
+class ContentFormView(ModelFormMixin):
 
     def get_form_class(self):
         """
@@ -84,58 +82,52 @@ class ContentUpdateView(ContentMixin, UpdateView):
     def form_valid(self, form):
         info = _("%s was successfully updated") % self.object.title
         messages.add_message(self.request, messages.INFO, info)
-        return super(ContentUpdateView, self).form_valid(form)
+        return super(ContentFormView, self).form_valid(form)
 
     def form_invalid(self, form):
         info = _("There is error in the form")
         messages.add_message(self.request, messages.ERROR, info)
-        return super(ContentUpdateView, self).form_invalid(form)
+        return super(ContentFormView, self).form_invalid(form)
 
-class ContentCreateView(ContentMixin, CreateView):
+
+class ContentUpdateView(ContentMixin, ContentFormView, UpdateView):
+
+    def get_template_names(self):
+        return ("ecm/%s_edit.html" % self.object.content_type.model,
+                "ecm/folder_edit.html",)
+
+
+class ContentCreateView(ContentMixin, ContentFormView, CreateView):
 
     def get_template_names(self):
         return ("ecm/%s_create.html" % self.model.__class__.__name__,
                 "ecm/folder_create.html",)
 
-    def get_form_class(self):
-        """
-        Returns the form class to use in this view
-        """
-        # TODO move & factorize
+    def _initialize(self):
         type = self.kwargs.get('content_type').lower()
         self.content_type = ContentType.objects.get(model=type)
         self.model = self.content_type.model_class()
         self._setup_settings(type)
-        
-        if self.form_class:
-            return self.form_class
-        else:
-            if self.model is not None:
-                # If a model has been explicitly provided, use it
-                model = self.model
-            elif hasattr(self, 'object') and self.object is not None:
-                # If this view is operating on a single object, use
-                # the class of that object
-                model = self.object.__class__
-            else:
-                # Try to get a queryset and extract the model class
-                # from that
-                model = self.get_queryset().model
-            return model_forms.modelform_factory(model,
-                    **{'exclude': self.settings.get_exclude()})
+
+    def get(self, request, *args, **kwargs):
+        self._initialize()
+        return super(ContentCreateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self._initialize()
+        return super(ContentCreateView, self).post(request, *args, **kwargs)
 
     def form_valid(self, form):
+        """
+        Create content using the context.
+        """
         parent_slug = self.kwargs.get('parent_slug')
         obj = form.save(commit=False)
         obj.content_type = self.content_type
         obj.parent = Catalog.objects.get(slug=parent_slug)
         obj.save()
         self.object = obj
+
         info = _("%s was successfully created") % obj.title
         messages.add_message(self.request, messages.INFO, info)
         return HttpResponseRedirect(self.get_success_url())
-
-    def form_invalid(self, form):
-        info = _("There is error in the form")
-        messages.add_message(self.request, messages.ERROR, info)
-        return super(ContentCreateView, self).form_invalid(form)
