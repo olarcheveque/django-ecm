ad1d2b96b32f32cd374bfbba7cb918dd4f16bb68
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-08-07 23:20:56 -0400

    user & group list

--- src/django-ecm/ecm/core/templatetags/ecm_tags.py
+++ src/django-ecm/ecm/core/templatetags/ecm_tags.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from ecm.core.models import Catalog
+
 from django import template
+
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from ecm.core.models import Catalog
 
 register = template.Library()
 
@@ -14,3 +19,16 @@ def show_navigation(context, level=1, types=None):
         nodes = [n for n in root.get_descendants() if n.content_type.model in
                 types]
         return {'nodes': nodes}
+
+@register.inclusion_tag('ecm/tags/navbar.html', takes_context=True)
+def show_navbar(context, ):
+    nodes = (
+            {'title': _("Users"),
+             'url': reverse('users_list'),
+             'children': (),
+             },
+            {'title': _("Groups"),
+             'url': reverse('groups_list'),
+             'children': (),
+             },)
+    return {'nodes': nodes}
--- src/django-ecm/ecm/core/urls.py
+++ src/django-ecm/ecm/core/urls.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from django.views.generic import ListView
 from django.conf.urls.defaults import patterns, url
+
+from django.contrib.auth.models import User, Group
+
 from views import ContentDetailView, ContentUpdateView, ContentCreateView
 
 urlpatterns = patterns('views',
+
+    # Users Management
+    url(r'users/$', ListView.as_view(
+        model=User,
+        template_name="ecm/users_list.html"),
+        name="users_list"),
+    url(r'groups/$', ListView.as_view(
+        model=Group,
+        template_name="ecm/groups_list.html"),
+        name="groups_list"),
+
+    # Entry point
     url(r'^$', ContentDetailView.as_view(), {'slugs': 'root'}),
 
-    # Root level
+    # Contents
     url(r'(?P<slugs>.*)/view$', ContentDetailView.as_view(),
         name="content_detail"),
     url(r'(?P<slugs>.*)/edit$', ContentUpdateView.as_view(),
@@ -14,4 +30,5 @@ urlpatterns = patterns('views',
     url(r'(?P<slugs>.*)/create/(?P<content_type>\w+)/$', ContentCreateView.as_view(),
     name="content_create"),
     url(r'(?P<slugs>.*)/$', ContentDetailView.as_view(),),
+
 )
