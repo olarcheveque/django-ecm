7293af07214c47f5237f96f40a6485a2f510369d
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-08-09 19:59:39 -0400

    be coherent with nodes

--- src/django-ecm/ecm/core/templatetags/ecm_tags.py
+++ src/django-ecm/ecm/core/templatetags/ecm_tags.py
@@ -10,13 +10,14 @@ from ecm.core.models import Catalog
 register = template.Library()
 
 @register.inclusion_tag('ecm/tags/navigation.html', takes_context=True)
-def show_navigation(context, level=1, types=None):
+def show_navigation(context, types=None):
     root = Catalog.objects.get(parent__isnull=True)
+    nodes = [root, ]
     if types is None:
-        nodes = [n for n in root.get_descendants()]
+        nodes += [n for n in root.get_descendants()]
         return {'nodes': nodes}
     else:
-        nodes = [n for n in root.get_descendants() if n.content_type.model in
+        nodes += [n for n in root.get_descendants() if n.content_type.model in
                 types]
         return {'nodes': nodes}
 
