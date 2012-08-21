fc6cc88fe59667ea37abe14ae625adeab716fed7
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-08-20 21:20:50 -0400

    rm

--- src/django-ecm/ecm/core/views/permissions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.views.generic import ListView
-
-from ecm.core.models import ECMPermission
-
-class PermissionListView(ListView):
-    model = ECMPermission
-    template_name = "ecm/permissions/list.html"
